f80ca60f10c20be18a6ade18720e2ddc2d222136
Kat Reinhart <kat.reinhart@gmail.com>
2013-10-06 16:43:15 -0700

    Udated secure token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '3a125d64c8620f7ae8f0802d6092a5e316f760880580a4c768d8458ad9e07a86f09c6ed64ee99cd7e2ee3a9af77a1bebea1213f0fb14f4d75df0172a7fa5b635'
+# SampleApp::Application.config.secret_key_base = '3a125d64c8620f7ae8f0802d6092a5e316f760880580a4c768d8458ad9e07a86f09c6ed64ee99cd7e2ee3a9af77a1bebea1213f0fb14f4d75df0172a7fa5b635'
+
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token.
+		File.read(token_file).chomp
+	else 
+		# Generate a new token and store it in token_file.
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+
